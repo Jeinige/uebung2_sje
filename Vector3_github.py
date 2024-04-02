4cf8729d576c4fa7f796e98627d494e862b96a1f
Jerome Seinige <162427357+Jeinige@users.noreply.github.com>
2024-04-02 17:16:53 +0200

    edits
    div. operatoren umdefiniert, radd, rsub, rmul, len, cross, dot und normalize hinzugefügt.
    alles ausgetestet

--- Vector3_github.py
+++ Vector3_github.py
@@ -7,29 +7,59 @@ class Vector3:
     def __str__(self):
         return (f"{self.x},{self.y},{self.z}")
 
-    def __radd__(self,other):
+    def __add__(self,other):
         if type(other)==int or type(other)==float:
             return Vector3(self.x+other, self.y+other,self.z+other)
         else:
             return Vector3(self.x+other.x,self.y+other.y,self.z+other.z)
 
-    def __rsub__(self,other):
+    def __sub__(self,other):
         if type(other)==int or type(other)==float:
             return Vector3(self.x-other, self.y-other,self.z-other)
         else:
             return Vector3(self.x-other.x,self.y-other.y,self.z-other.z)
         
-    def __rmul__(self,other):
+    def __mul__(self,other):
         if type(other)==int or type(other)==float:
             return Vector3(self.x*other, self.y*other,self.z*other)
         else:
             return Vector3(self.x*other.x,self.y*other.y,self.z*other.z)
+        
+    def __radd__(self, other):
+        return Vector3(self.x+other,self.y+other,self.z+other)
 
-    def Skalarprodukt(self, other):
-        if type(other)==int
+    def __rsub__(self, other):
+        return Vector3(other-self.x,other-self.y,other-self.z)
+
+    def __rmul__(self, other):
+        return Vector3(self.x*other,self.y*other,self.z*other)
+
+    def len(self):
+        return (self.x**2+self.y**2+self.z**2)**0.5
+
+    def cross(self, other):
+        return Vector3(self.y*other.z-self.z*other.y,self.z*other.x-self.x*other.z,self.x*other.y-self.y*other.x)
+    
+    def dot(self, other):
+        return (self.x*other.x+self.y*other.y+self.z*other.z)
+
+    def normalize(self):
+        return Vector3(self.x/self.len(),self.y/self.len(),self.z/self.len())
+
 a = Vector3(3,4,2) 
 b = Vector3(2,1,0) 
-c = a * b      # Komponentenweise Multiplikation 
-print(c) 
-d = a.dot(b)   # Skalarprodukt 
-e = a.cross(b) # Kreuzprodukt 
+
+print(a)
+print(3+a)
+print(a+3)
+print(a+b)
+print(a-b)
+print(3-a)
+print(a-3)
+print(a-b)
+print(a*b)
+print(a*3)
+print(a.cross(b))
+print(a.dot(b))
+print(a.normalize())
+
